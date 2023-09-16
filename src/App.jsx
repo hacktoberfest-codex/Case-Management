ccd696238fd96adc9bb923ad49dc72b4d55343ae
Shohail Parwej <shohailparwej@gmail.com>
2023-09-17 03:04:02 +0530

    add routes for webpages

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,26 @@ import Nav from "./components/Nav"
 import Banner from "./components/Banner"
 import About from "./components/About"
 import Vision from "./components/Vision"
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import Home from "./Home"
 
 function App() {
 
   return (
     <>
-      <div className="overflow-x-hidden">
+<BrowserRouter>
+<Routes>
+<Route path="/home" element={<Home />}/>
+</Routes>
+</BrowserRouter>
+{/**     <div className="overflow-x-hidden">
         <Nav />
         <hr />
         <Banner />
         <About />
         <Vision />
         </div>
+  */}
     </>
   )
 }

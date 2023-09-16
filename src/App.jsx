a41dcb630f1f1173b52c8a80dbbe671c4202a523
Raj Aryan <rajisonline2306@gmail.com>
2023-09-16 19:17:20 +0530

    Add navbar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Nav from "./components/Nav"
+
+function App() {
+
+  return (
+    <>
+      <div className="overflow-x-hidden">
+        <Nav />
+        <hr />
+        </div>
+    </>
+  )
+}
+
+export default App
